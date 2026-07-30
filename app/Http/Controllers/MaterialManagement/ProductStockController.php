067fbce532a8debb1261bb2ff46dddfb4b704201
DKcyp <txd1k47x7@gmail.com>
2026-07-30 15:26:54 +0700

    feat: seed Product Stock dummy data - 25 products × multi-warehouse

--- app/Http/Controllers/MaterialManagement/ProductStockController.php
+++ app/Http/Controllers/MaterialManagement/ProductStockController.php
@@ -15,7 +15,67 @@ class ProductStockController extends Controller
     public function __construct()
     {
         $this->store = new DummyStore('material-stock');
+        $this->initDummyData();
         View::share('activeMenu', 'material-management');
+    }
+
+    protected function initDummyData(): void
+    {
+        if (!empty($this->store->all())) return;
+
+        $products = [
+            ['product_id' => 'PRD-BB-0001', 'name' => 'Resin Polyester White', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0002', 'name' => 'Resin Epoxy Clear', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0003', 'name' => 'Talc Powder 400 Mesh', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0004', 'name' => 'Titanium Dioxide R-706', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0005', 'name' => 'Calcium Carbonate', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0006', 'name' => 'Silica Sand', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0007', 'name' => 'Pigment Oxide Red', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0008', 'name' => 'Pigment Carbon Black', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0009', 'name' => 'Pigment Yellow Oxide', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0010', 'name' => 'Alkyd Resin Medium', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0011', 'name' => 'Acrylic Emulsion', 'category' => 'Bahan Baku', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BB-0012', 'name' => 'Mineral Spirit', 'category' => 'Bahan Baku', 'uom' => 'L'],
+            ['product_id' => 'PRD-BN-0001', 'name' => 'Thinner A Special', 'category' => 'Penolong', 'uom' => 'L'],
+            ['product_id' => 'PRD-BN-0002', 'name' => 'Defoamer AF-200', 'category' => 'Penolong', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BN-0003', 'name' => 'Dispersing Agent', 'category' => 'Penolong', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BN-0004', 'name' => 'Anti-Rust Agent', 'category' => 'Penolong', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-BN-0005', 'name' => 'Wax Emulsion', 'category' => 'Penolong', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-WP-0001', 'name' => 'Wall Paint White Base', 'category' => 'WIP', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-WP-0002', 'name' => 'Wall Paint Cream Base', 'category' => 'WIP', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-WP-0003', 'name' => 'Primer Grey Base', 'category' => 'WIP', 'uom' => 'Kg'],
+            ['product_id' => 'PRD-FG-0001', 'name' => 'Wall Paint White 20L', 'category' => 'Finished Goods', 'uom' => 'Pcs'],
+            ['product_id' => 'PRD-FG-0002', 'name' => 'Wall Paint Cream 10L', 'category' => 'Finished Goods', 'uom' => 'Pcs'],
+            ['product_id' => 'PRD-FG-0003', 'name' => 'Primer Grey 5L', 'category' => 'Finished Goods', 'uom' => 'Pcs'],
+            ['product_id' => 'PRD-FG-0004', 'name' => 'Top Coat Clear 15L', 'category' => 'Finished Goods', 'uom' => 'Pcs'],
+            ['product_id' => 'PRD-FG-0005', 'name' => 'Cat Ekonomis 5L', 'category' => 'Finished Goods', 'uom' => 'Pcs'],
+        ];
+
+        $warehouses = ['Gudang Bahan Bandung','Gudang Bahan Jakarta','Gudang WIP Bandung','Gudang Jadi Bandung','Gudang Jadi Jakarta'];
+
+        foreach ($products as $p) {
+            $whCount = $p['category'] === 'WIP' ? 1 : ($p['category'] === 'Finished Goods' ? 2 : 3);
+            $whSlice = array_slice($warehouses, 0, $whCount);
+            foreach ($whSlice as $wh) {
+                $cur = match($p['category']) {
+                    'Bahan Baku' => rand(200, 5000),
+                    'Penolong' => rand(100, 1500),
+                    'WIP' => rand(50, 800),
+                    'Finished Goods' => rand(20, 500),
+                    default => rand(100, 1000),
+                };
+                $res = rand(0, intdiv($cur, 4));
+                $this->store->create([
+                    'product_id' => $p['product_id'],
+                    'name' => $p['name'],
+                    'category' => $p['category'],
+                    'warehouse' => $wh,
+                    'current_stock' => $cur,
+                    'reserved_stock' => $res,
+                    'uom' => $p['uom'],
+                ]);
+            }
+        }
     }
 
     public function index()
